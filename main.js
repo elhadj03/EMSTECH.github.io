ee1995c51ff16d80e80e7e09cbd0d471640c0ef5
elhadj03 <60812323+elhadj03@users.noreply.github.com>
2022-06-26 00:11:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "CAPGEMINI", "ENGINEERING", "France"],
+        strings: [ "CAPGEMINI", "ENGINEERING", "FRANCE"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
