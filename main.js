d153b8df37c3b741863ac727cb1150e540ead16b
elhadj03 <60812323+elhadj03@users.noreply.github.com>
2022-06-26 00:33:59 +0200

    Update main.js

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["ELI LILLY", "FRANCE"],
+        strings: ["ELI LILLY & CO", "FRANCE"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
